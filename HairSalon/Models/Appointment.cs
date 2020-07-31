9abcd0ecde7fb516b58bf953a4abbb29b74022c0
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-31 15:58:49 -0700

    change Appointment and Client classes to depends on each other

--- HairSalon/Models/Appointment.cs
+++ HairSalon/Models/Appointment.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
   {
     public int AppointmentId { get; set; }
     public string Name {get; set;}
-    public string Date {get; set;}
+    public DateTime Date {get; set;}
     public string Time {get; set;}
     public int ClientId { get; set; }
     public virtual Client Client { get; set; }
--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -4,6 +4,10 @@ namespace HairSalon.Models
 {
   public class Client
   {
+    public Client()
+    {
+      this.Appointments = new HashSet<Appointment>();
+    }
     public int ClientId { get; set; }
     public string FirstName {get; set;}
     public string LastName {get; set;}
@@ -11,5 +15,7 @@ namespace HairSalon.Models
     public string FullName {get {return FirstName + " " + LastName;}}
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
+    public virtual ICollection<Appointment> Appointments { get; set; }
+
   }
 }
