63aceb32626801ec963736597ecea7736217fcfe
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-31 16:30:20 -0700

    add DbSet Appointments

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -4,6 +4,7 @@ namespace HairSalon.Models
 {
   public class HairSalonContext : DbContext
   {
+    public virtual DbSet<Appointment> Appointments { get; set; }
     public virtual DbSet<Stylist> Stylists { get; set; }
     public DbSet<Client> Clients { get; set; }
     public HairSalonContext(DbContextOptions options) : base(options) { }
