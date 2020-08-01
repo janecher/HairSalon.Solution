285d2b19b72f62f92e118dc2d465ba7754642537
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-07-31 17:00:01 -0700

    add DateInfo property

--- HairSalon/Models/Appointment.cs
+++ HairSalon/Models/Appointment.cs
@@ -7,6 +7,7 @@ namespace HairSalon.Models
     public int AppointmentId { get; set; }
     public string Name {get; set;}
     public DateTime Date {get; set;}
+    public string DateInfo {get {return Date.ToString("mm/dd/yyyy");}}
     public string Time {get; set;}
     public int ClientId { get; set; }
     public virtual Client Client { get; set; }
